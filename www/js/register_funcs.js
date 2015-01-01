02dcd9912d05c607aeadbfaa9d5273dbcde5b6c4
Ángel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2015-01-01 12:15:31 +0100

    changes in register

--- www/js/register_funcs.js
+++ www/js/register_funcs.js
@@ -1,12 +1,24 @@
+function initialize_boxes()
+{
+  user_id = getCookie("user_id");
+  document.getElementById("current_ID").value = user_id;
+}
+ 
+function new_ID()
+{
+  value = Math.floor(Math.random()*100000000000000);
+  document.getElementById("current_ID").value = value;
+  deleteCookie("user_id");
+  setCookie("user_id",value,0);
+}
 
-function init_register_boxes()
-  {
-    document.getElementById("newuser_textinput").value = "Sick Bastard"; 
+function save_userdata()
+{
+  user_id = document.getElementById("current_ID").value;
+  user_name = document.getElementById("name_input").value;
+  if (user_name == "Enter your name here" || user_name == "") {
+    user_name = "Anonymous, I guess";
   }
-
-function sendNewUser()
-  {
-    value = document.getElementById("newuser_textinput").value;
-    alert(value); 
-  }
+  alert(user_id + " - " + user_name);
+}
 
