1273a5d21c1eb5f3ce23eafde64de872d6edafae
Ángel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2015-01-01 09:02:44 +0100

    first commit

---
+++ platforms/android/assets/www/js/register_funcs.js
@@ -0,0 +1,12 @@
+
+function init_register_boxes()
+  {
+    document.getElementById("newuser_textinput").value = "Sick Bastard"; 
+  }
+
+function sendNewUser()
+  {
+    value = document.getElementById("newuser_textinput").value;
+    alert(value); 
+  }
+
---
+++ www/js/main_funcs.js
@@ -0,0 +1,46 @@
+
+function initialize_boxes(username)
+  {
+    server='192.168.10.212:8000';
+    $.ajax({
+      url: 'http://' + server + '/user/1',
+      success: function(result) {
+        document.getElementById("current_pokes").innerHTML = result;
+      },
+      error: function(e,result) {
+        alert(result + e); 
+      }
+    });
+    user_id = getCookie("user_id");
+    document.getElementById("current_ID").innerHTML = user_id;
+  }
+
+function addone(username)
+  {
+    server='192.168.10.212:8000';
+    $.ajax({
+      url: 'http://' + server + '/poke/1/1',
+      success: function(result) {
+        initialize_boxes(username);
+      },
+      error: function(e,result) {
+        alert(result + e);
+      }
+    });
+
+  }
+
+function cleanall(username)
+  {
+    server='192.168.10.212:8000';
+    $.ajax({
+      url: 'http://' + server + '/cleanall/1',
+      success: function(result) {
+        initialize_boxes(username);
+      },
+      error: function(e,result) {
+        alert(result + e);
+      }
+    });
+
+  }
